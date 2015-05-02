465f044942830c844b9811ab4d858d25ec44f88b
John Yasmer <jyasmer@gmail.com>
2015-05-01 22:30:25 -0700

    First commit! Add templates and static files

---
+++ bobaed/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bobaed project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bobaed.settings")
+
+application = get_wsgi_application()
